15b997b2b3e10fff51089691d0dd9bc586a90cf3
YithSopheaktra8 <yithsopheaktra18@gmail.com>
2024-02-17 17:48:26 +0700

    customize tailwind font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ["./**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				roboto: ["Roboto", "sans-serif"],
+				logo : ["Fontdiner Swanky", "cursive"]
+			},
+			colors :{
+				primary : "#0D53CF",
+				background : {
+					white : "#FDFDFD",
+					blue : "#E7EEFA"
+				},
+				text : {
+					black : "#343434",
+					black80 : "#484848",
+					black10 : "#717171",
+				}
+			},
+		},
 	},
 	plugins: [require("flowbite/plugin")],
 };
