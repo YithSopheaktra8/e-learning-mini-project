4786bedbb93daf05ceb9aa6b55b3d6ffbf17a420
Yith Sopheaktra <yithsopheaktra18@gmail.com>
2024-02-20 19:19:00 +0700

    rename image

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
-const slider = document.querySelector('#slider');
+const Aos = require("aos");
+
+const slider = document.querySelector("#slider");
 
 function onSlideLeft() {
-    slider.scrollLeft -= 300;
+	slider.scrollLeft -= 300;
 }
 
 function onSlideRight() {
-    slider.scrollLeft += 300;
+	slider.scrollLeft += 300;
 }
